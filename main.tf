bf30984fea19328718f27a11d2ee6c0d50eca1c6
Javier Almeida <108470987+javieralmeida30@users.noreply.github.com>
2023-05-29 18:40:05 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "us-east-1"
-}
+} #Region en la que se encuentra la ami seleccionada
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-sg"
@@ -9,21 +9,21 @@ resource "aws_security_group" "instance" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #dar paso desde cualquier ip
   }
   
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1" #-1 dar salida a cualquier lugar
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "web" {
-  ami = "ami-06a0cd9728546d178"
+  ami = "ami-06a0cd9728546d178" #ami de la region elegida
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.instance.id] # grupo de seguridad por defecto
 
   user_data = <<-EOF
               #!/bin/bash
@@ -32,6 +32,6 @@ resource "aws_instance" "web" {
               EOF
 
   tags = {
-    Name = "Hola Mundo"
+    Name = "Hola LinkedIn"
   }
 }
